319f2d77badf9ee9d31084e35f34c15d42cc7618
David Vidal <dvidal@infoyupay.com>
2025-12-18 16:36:00 -0500

    build: enrich POM metadata for Maven Central release

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,51 @@ plugins {
 group = "com.infoyupay.humandate"
 version = "1.0.0"
 
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set("humandate-fx")
+                description.set(
+                    "JavaFX adapter library for humandate-core, providing " +
+                            "human-friendly date bindings, converters and UI helpers."
+                )
+                url.set("https://github.com/infoyupay/humandate-fx")
+
+                licenses {
+                    license {
+                        name.set("Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("dvidal")
+                        name.set("David Vidal")
+                        email.set("dvidal@infoyupay.com")
+                        organization.set("InfoYupay")
+                        organizationUrl.set("https://infoyupay.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/infoyupay/humandate-fx.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:infoyupay/humandate-fx.git")
+                    url.set("https://github.com/infoyupay/humandate-fx")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
